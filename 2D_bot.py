4146aa670f895762395ab66c3267dce682378985
qqDesu <qqtred@yandex.ru>
2022-05-18 13:16:19 +0300

    fixed markup, added screen buttons

--- 2D_bot.py
+++ 2D_bot.py
@@ -2,8 +2,13 @@ import requests
 import telebot
 from telebot import types
 import json
+from configparser import ConfigParser
 
-bot = telebot.TeleBot('5038025851:AAHm4BV1V6VYrw-8rT2l1dRH35yfYADX0PM')
+parser = ConfigParser()
+parser.read("config.txt")
+token = parser.get("auth", "token")
+
+bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(commands=['start'],content_types='text')
@@ -15,15 +20,15 @@ def help_message(message):
     bot.send_message(message.chat.id, "Type '/waifu', baka! (・`ω´・)")
     
 @bot.message_handler(commands=['waifu'])
-def button_message(message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+def main_category_select(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) #, one_time_keyboard=True
     categories = ['sfw', 'nsfw']
     keyboard.add(*categories)
-    bot.send_message(message.chat.id, "Category?", reply_markup=keyboard)
-    bot.register_next_step_handler(message, category_select)
+    bot.send_message(message.chat.id, "Category?", reply_markup=keyboard) 
+    bot.register_next_step_handler(message, subcategory_select)
 
-def category_select(message):    
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+def subcategory_select(message):    
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True) #, one_time_keyboard=True
     
     if message.text == 'sfw':
         global category
@@ -49,11 +54,14 @@ def waifu_url(message):
     subcategory = message.text
     if subcategory == 'Back to main menu':
         bot.send_message(message.chat.id, "Make up your mind already! BAKAYARO ヽ( `д´*)ノ")
+        main_category_select(message)
+        
         
     else:
         response = requests.get(('https://api.waifu.pics/{}/{}').format(category, subcategory))
         json_url = response.json()
         waifu = json_url['url']
         bot.send_photo(message.chat.id, photo=waifu)
+        main_category_select(message)
 
 bot.polling(non_stop=True, interval=0)
